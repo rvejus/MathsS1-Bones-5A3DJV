a551db0e1db7dee5957d453b1476352ae8f754fe
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2023-02-09 18:23:35 +0100

    Update MakeBoneGO.cs
    Variance & init matrix

--- Maths-Bones/Assets/Scripts/MakeBoneGO.cs
+++ Maths-Bones/Assets/Scripts/MakeBoneGO.cs
@@ -7,10 +7,14 @@ public class MakeBoneGO : MonoBehaviour
     public GameObject prefabBary;
     public List<GameObject> goPoints = new List<GameObject>();
     public GameObject bary;
+    public Vector3 OG=Vector3.zero;
+    public List<List<float>> covarMat = new List<List<float>>();
     void Start()
     {
         calculBarycentre();
         recenter();
+        initCovarMat();
+        covarMatCalcul();
     }
 
     public void calculBarycentre()
@@ -24,15 +28,57 @@ public class MakeBoneGO : MonoBehaviour
         barypos /= goPoints.Count;
         bary = Instantiate(prefabBary, barypos, Quaternion.Euler(Vector3.zero));
     }
-
     public void recenter()
     {
-        Vector3 OG = Vector3.zero + bary.transform.position;
+        OG = Vector3.zero + bary.transform.position;
         bary.transform.position = Vector3.zero;
         foreach (GameObject go in goPoints)
         {
             go.transform.position -= OG;
         }
     }
-    
+    public void initCovarMat()
+    {
+        covarMat.Add(new List<float>(3));
+        covarMat.Add(new List<float>(3));
+        covarMat.Add(new List<float>(3));
+    }
+    public float variance(string angle)
+    {
+        float Var = 0;
+        float moyenne = 0;
+        if (angle == "x")
+        {
+            foreach (GameObject go in goPoints)
+            {
+                moyenne += go.transform.position.x;
+                Var += Mathf.Pow(go.transform.position.x, 2);
+            }
+            
+        }else if (angle == "y")
+        {
+            foreach (GameObject go in goPoints)
+            {
+                moyenne += go.transform.position.y;
+                Var += Mathf.Pow(go.transform.position.y, 2);
+            }
+            
+        }else if (angle == "z")
+        {
+            foreach (GameObject go in goPoints)
+            {
+                moyenne += go.transform.position.z;
+                Var += Mathf.Pow(go.transform.position.z, 2);
+            }
+            
+        }
+        moyenne /= goPoints.Count;
+        Var /= goPoints.Count;
+        Var -= Mathf.Pow(moyenne, 2);
+        return Var;
+    }
+    public void covarMatCalcul()
+    {
+        
+    }
 }
